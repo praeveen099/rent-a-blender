98bc5ec01b09f7ebaf92f7cf719950ca090206a5
Praeveen Ganesharaja <praeveen099@gmail.om>
2022-07-08 12:26:41 +0100

    Adding css to the index.php

--- index.php
+++ index.php
@@ -4,11 +4,21 @@
   <meta charset="utf-8">
   <title>Rent a Blender</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel ="stylesheet" href="css/style.css">
 </head>
 <body>
   <main>
     <header>
-      
+      <div class="main-nav">
+        <ul class="nav">
+            <li class="name">Rent A Blender</li>
+            <li>Amazon</li>
+            <li>Argos</li>
+            <li>EBay</li>
+            <li>Rent</li>
+            <li>Contact Us</li>
+        </ul>
+      </div>
     </header>
 
 
