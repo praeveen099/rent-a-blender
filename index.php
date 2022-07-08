901713aa4db9bb919579bdd6f04fe040ad3804bd
Praeveen Ganesharaja <praeveen099@gmail.om>
2022-07-08 10:26:38 +0100

    Starting file for the website

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Rent a Blender</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <main>
+    <header>
+      
+    </header>
+
+
+    <footer>
+
+    </footer>
+  </main>
+</body>
+</html>
