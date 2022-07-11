bc3cfb24f47c5455b15acef9209752e51576fe9f
Praeveen Ganesharaja <praeveen099@gmail.om>
2022-07-11 07:22:29 +0100

    Changed CSS file used to test that there is a margin between header, footer and main

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
   <meta charset="utf-8">
   <title>Rent a Blender</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel ="stylesheet" href="css/style.css">
+<!--  <link rel ="stylesheet" href="css/style.css"> -->
+  <link rel ="stylesheet" href="css/styles.css">
 </head>
 <body>
 <?php include("heading.php");?>
@@ -14,6 +15,7 @@
   <main>
   </main>
   <footer>
+    <p class = "Copyright">&copy; <a href = "https://github.com/praeveen099"> Praeveen Ganesharaja </a> <?php echo date("Y"); ?> </p>
   </footer>
 
 </body>
