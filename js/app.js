340204f95e3bd004856966e8cb5285e980a1aca4
Praeveen Ganesharaja <praeveen099@gmail.om>
2022-07-15 16:25:18 +0100

    Added a button that appears when the mouse hovers the images of the blender

--- js/app.js
+++ js/app.js
@@ -4,14 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
   ul.addEventListener('mouseover', (e)=> {
     if (e.target.tagName === "IMG"){
-      const imageMouseIsOver = e.target;
-      // blur the image when the mouse hovers over
-      imageMouseIsOver.classList.add('blur');
+        const imageMouseIsOver = e.target;
+        // blur the image when the mouse hovers over
+        imageMouseIsOver.classList.add('blur');
 
-      // after 5 seconds, remove blur
-      setTimeout(()=>{
-      imageMouseIsOver.classList.remove('blur');
-    }, 5000);
+
+        // Create a button
+        const buttonAttachedToImage = document.createElement("BUTTON");
+        buttonAttachedToImage.textContent = "Click here to check the blender out!";
+
+        if (imageMouseIsOver.parentNode.children[1].nodeName !== "BUTTON"){
+
+        buttonAttachedToImage.classList.add('btn');
+        imageMouseIsOver.parentNode.insertBefore(buttonAttachedToImage, imageMouseIsOver.nextSibling);
+
+        }
+        // after 2 seconds, remove blur
+        setTimeout(()=>{
+        imageMouseIsOver.classList.remove('blur');
+        buttonAttachedToImage.remove();
+      }, 2000);
     }
 
   });
